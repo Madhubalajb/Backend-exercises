84e2b1b3d603f9b85e3fb1f397e2ab0e100915e2
Madhubalajb <jbmadhumng@gmail.com>
2019-06-27 19:47:36 +0530

    Added 3.6 Phonebook backend

--- index.js
+++ index.js
@@ -56,6 +56,22 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+    const filter = persons.some(person => person.name === body.name)
+    if (!body.name) {
+        return response.status(400).json({
+            error: "Name is missing"
+        })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: "Number is missing"
+        })
+    }
+    if (filter) {
+        return response.status(400).json({
+            error: "Name should be unique"
+        })
+    }
     const person = {
         id: persons.length + 1,
         name: body.name,
