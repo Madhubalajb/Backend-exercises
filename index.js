e2ca4ee604450c0f2cfc5e1c3e32f90cb114b57f
Madhubalajb <jbmadhumng@gmail.com>
2019-06-27 17:04:39 +0530

    added 3.1 Phonebook backend

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const PORT = 3001
+
+let persons = [
+    {
+        id: 1,
+        name: "Madhubala",
+        number: "8907834567"
+    },
+    {
+        id: 2,
+        name: "Jayavarshini",
+        number: "8345671234"
+    },
+    {
+        id: 3,
+        name: "Bharathi",
+        number: "9345671234"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Phone Book</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
