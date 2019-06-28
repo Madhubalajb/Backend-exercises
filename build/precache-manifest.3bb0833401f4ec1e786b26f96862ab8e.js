02b1120ff4aa4adc3f3a16c95fd7737a16688222
Madhubalajb <jbmadhumng@gmail.com>
2019-06-28 14:32:33 +0530

    Add production build to backend

---
+++ build/precache-manifest.3bb0833401f4ec1e786b26f96862ab8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8622ad157397b21aec38d109715679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cea37502256427cc8de",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "9cea37502256427cc8de",
+    "url": "/static/js/main.e372dee6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
