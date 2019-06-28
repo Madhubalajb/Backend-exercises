a9cb50e3cdbd42ac662db34f5f07333d6eea6b8d
Madhubalajb <jbmadhumng@gmail.com>
2019-06-28 20:48:29 +0530

    uibuild

--- build/precache-manifest.ae1907decc33884a1ef400c1c4cdef5e.js
+++ build/precache-manifest.ae1907decc33884a1ef400c1c4cdef5e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d699bfe98ea9976e6d6c218a35369dd8",
+    "revision": "f88b315d741ecd7c1fd2cc0916bd4e03",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "afdbd651b5b82b2fc870",
-    "url": "/static/css/main.7391d1af.chunk.css"
+    "revision": "14274219b54bb75da585",
+    "url": "/static/css/main.ee1442f4.chunk.css"
   },
   {
     "revision": "6f5c565559b8e6c1ccf1",
     "url": "/static/js/2.2f115dc6.chunk.js"
   },
   {
-    "revision": "afdbd651b5b82b2fc870",
-    "url": "/static/js/main.b0e4b689.chunk.js"
+    "revision": "14274219b54bb75da585",
+    "url": "/static/js/main.f0dfa247.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c003243fcb5b7411d14cc570220955f.js"
+  "/precache-manifest.ae1907decc33884a1ef400c1c4cdef5e.js"
 );
 
 self.addEventListener('message', (event) => {
