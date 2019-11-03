0de63bcfb5c7ad9dfcdec7e279493a953b0682c2
Madhubalajb <jbmadhumng@gmail.com>
2019-11-03 12:04:42 +0530

    ui-build

---
+++ build/precache-manifest.026f9680ca89160af46b7875f67055a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d22dee36e5ff0dee0d6059ab5266d0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d682b51cca6e76658e3b",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "32b60882cdcb5bb1fe05",
+    "url": "/static/css/main.58476d85.chunk.css"
+  },
+  {
+    "revision": "d682b51cca6e76658e3b",
+    "url": "/static/js/2.e533fb3b.chunk.js"
+  },
+  {
+    "revision": "32b60882cdcb5bb1fe05",
+    "url": "/static/js/main.ebbfab91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55d8da507737d12991f9ccab9dd9b55b",
+    "url": "/static/media/phone-book.55d8da50.png"
+  }
+]);
